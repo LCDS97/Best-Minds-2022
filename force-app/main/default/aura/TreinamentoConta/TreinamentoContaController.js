eee41479a5afe444f3b8fc20a81762934fb24302
lcds97 <lucas.cds1997@gmail.com>
2022-05-12 11:57:53 -0300

    Terminado aula de Aura Components com dois componentes de Conta e de Home

--- force-app/main/default/aura/TreinamentoConta/TreinamentoContaController.js
+++ force-app/main/default/aura/TreinamentoConta/TreinamentoContaController.js
@@ -1,5 +1,46 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+
+        let action = component.get("c.getAccount");
+
+        action.setParams({id : component.get("v.recordId")});
+
+        action.setCallback(this, function(response){
+            if(response.getState() == "SUCCESS"){
+                component.set("v.valorConta", response.getReturnValue());
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    salvarConta : function(component){
+
+        var action = component.get("c.updateAccount");
+
+        var recordId = component.get("v.recordId");
+
+        var nomedaConta = component.find("nomeConta").get("v.value");
+
+        action.setParams({Id : recordId, nomeAtualizado : nomedaConta});
+
+        action.setCallback(this, function(response){
+
+            if(response.getState() == "SUCCESS"){
+
+                $A.get('e.force:refreshView').fire();
+
+            }else{
+
+                alert('Falha ao atualizar o registro');
+
+            }
+
+        });
+
+
+
+        $A.enqueueAction(action);
 
     }
 })
